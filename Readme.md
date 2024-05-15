e3853d058b54fd6ec3e5f8932e9b42bf729ed934
ARJUN SHAJI <arjun.shaji@techgentsia.com>
2024-05-15 09:50:27 +0530

    -- simple vocal extration server from audio

---
+++ Readme.md
@@ -0,0 +1,4 @@
+Install dependency in requirements.txt
+    extra install tenserflow and numpy version = 1.23.5
+
+run python voiceolation.py <Audio Path>
---
+++ config.py
@@ -0,0 +1,10 @@
+#SR =  11025
+SR = 9000
+window_size = 1024
+hop_length = 256
+patch_size = 128 
+
+## for training
+EPOCH = 3
+BATCH = 32
+SAMPLING_STRIDE = 50
